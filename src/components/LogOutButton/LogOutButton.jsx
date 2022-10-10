34021cf2deba452370f46078e67366651adda1d5
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-10 12:52:23 -0500

    change log out button to mui

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import Button from '@mui/material/Button';
 
-function LogOutButton(props) {
+// function LogOutButton(props) {
+
+function LogOutButton() {
   const dispatch = useDispatch();
   return (
-    <button
-      // This button shows up in multiple locations and is styled differently
-      // because it's styled differently depending on where it is used, the className
-      // is passed to it from it's parents through React props
-      className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT' })}
-    >
-      Log Out
-    </button>
+    // <button
+    //   // This button shows up in multiple locations and is styled differently
+    //   // because it's styled differently depending on where it is used, the className
+    //   // is passed to it from it's parents through React props
+    //   className={props.className}
+    //   onClick={() => dispatch({ type: 'LOGOUT' })}
+    // >
+    //   Log Out
+    // </button>
+
+    <Button onClick={() => dispatch({ type: 'LOGOUT' })} variant="outlined">Log Out</Button>
   );
 }
 
