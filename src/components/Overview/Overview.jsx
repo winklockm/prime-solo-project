2d4a2eee2d175fa056ed15419ad046950666e1ac
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-10 09:11:34 -0500

    basic container and stack layout

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,4 +1,6 @@
 import LogOutButton from '../LogOutButton/LogOutButton';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 
 function Overview() {
 
@@ -6,12 +8,20 @@ function Overview() {
 
     return (
         <>
-            <p>This is the Overview component</p>
-            <p>You have:</p>
-            <p># medical providers</p>
-            <p># medications</p>
-            <p># insurance plans</p>
-            <LogOutButton className="navLink" />
+
+            <Container maxWidth="sm">
+                <Stack spacing={2}>
+                    <p>This is the Overview component</p>
+                    <p>You have:</p>
+                    <p># medical providers</p>
+                    <p># medications</p>
+                    <p># insurance plans</p>
+                    <LogOutButton className="navLink" />
+                </Stack>
+
+
+            </Container>
+
         </>
     )
 }
