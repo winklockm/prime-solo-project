441c4ec0df32d51a1320a752bb9a1ab2bcdb82f4
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-10 18:29:12 -0500

    use reducer for image upload

--- src/components/ImageUpload/ImageUpload.jsx
+++ src/components/ImageUpload/ImageUpload.jsx
@@ -7,31 +7,17 @@ import Stack from '@mui/material/Stack';
 
 
 function ImageUpload() {
-    // const [image, setImage] = useState('');
-    // const dispatch = useDispatch();
-    // const patientImage = useSelector((store) => store.patient);
-
-    // const uploadImage = () => {
-    //     console.log('in uploadImage. here is image:', image);
-    //     dispatch({
-    //         type: 'SET_PATIENT_IMAGE',
-    //         payload: image
-    //     })
-    // }
-
-
-    //above is working
     const [image, setImage] = useState('');
-    const [url, setURL] = useState('');
-    // const dispatch = useDispatch();
-    // const patientImage = useSelector((store) => store.patient);
+    const dispatch = useDispatch();
+    const patientImage = useSelector((store) => store.patient);
 
-    const previewImage = event => {
-        event.preventDefault();
-        console.log('in previewImage. here is image:', image);
-        setImage(url)
+    const uploadImage = () => {
+        console.log('in uploadImage. here is image:', image);
+        dispatch({
+            type: 'SET_PATIENT_IMAGE',
+            payload: image
+        })
     }
-
 
     return (
         <Stack spacing={2} 
@@ -39,32 +25,20 @@ function ImageUpload() {
         justifyContent="center"
         alignItems="center"
         >
-        
             <TextField
             id="filled-basic" 
             label="url" 
             variant="filled" 
             required
-            onChange={(event) => setURL(event.target.value)}
+            onChange={(event) => setImage(event.target.value)}
             />
-            <Button onClick={previewImage} variant="contained">Image Preview</Button>
-
-            {/* show image only if there is an image set */}
-            {image && (
-            <img src={image} className='imagePreview'></img>
-            )}
-
-
-
-        {/* <TextField
-        id="filled-basic" 
-        label="url" 
-        variant="filled" 
-        required
-        onChange={(event) => setImage(event.target.value)}
-        />
+            
             <Button onClick={uploadImage} variant="contained">Image Preview</Button>
-            <img src={patientImage}></img> */}
+  
+            {/* show image only if there is an patientImage set */}
+            {patientImage && (
+            <img src={patientImage} className='imagePreview'></img>
+            )} 
         </Stack>
     )
 }
