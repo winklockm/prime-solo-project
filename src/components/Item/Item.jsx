b110a19316273b4df7276c956fbc3e31a66fe72e
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-18 15:30:11 -0500

    mui card styling for list and item components

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,44 +2,34 @@ import Grid from '@mui/material/Unstable_Grid2';
 import {useHistory} from 'react-router-dom';
 import './Item.css';
 import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function Item({provider}) {
 
     const history = useHistory();
 
-const handleClick = () => {
+    const handleClick = () => {
     console.log('in handleClick. Provider.id is:', provider.id);
     history.push(`/medicalteam/detail/${provider.id}`);
 } 
 
     return (
-        <Container className='itemContainer' maxWidth="sm">
-        <Grid
-        spacing={0}
-        className='itemGrid'
-        container
-        alignItems="stretch"
-        justifyContent='center'
-        key={provider.id}
-        onClick={handleClick}
-        >
-            <Grid 
-            className='itemName'
-            xs={4}>
-                <p>{provider.name}</p>
-            </Grid>
-            <Grid 
-            className='itemSpecialty'
-            xs={4}>
-                <p>{provider.specialty}</p>
-            </Grid>
-            <Grid 
-            className='itemClinic'
-            xs={4}>
-                <p>{provider.clinic}</p>
-            </Grid>
-        </Grid>
-        </Container>
+            <Card 
+            sx={{ minWidth: 300 }} 
+            key={provider.id}
+            onClick={handleClick}
+            >
+                <CardContent>
+                <Typography>{provider.name}</Typography>
+                <Typography variant="body2" >{provider.specialty}</Typography>
+                <Typography variant="body2" color="text.secondary">{provider.clinic}</Typography>
+                </CardContent>
+            </Card>
+       
     )
 }
 
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -6,6 +6,9 @@ import Button from '@mui/material/Button';
 import Item from "../Item/Item";
 import Stack from '@mui/material/Stack';
 import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+import AddIcon from '@mui/icons-material/Add';
+
 import './List.css';
 
 function List() {
@@ -30,22 +33,17 @@ function List() {
                 alignItems="center"
                 spacing={2}
             >
-                <p className="listComponentTitle">Medical Providers</p>
-
+                <Typography>Medical Providers</Typography>
                 {
                     medicalteam.length > 0 ?
-                        <Grid 
-                        className='listGrid'
-                        container 
-                        alignItems="stretch"
-                        spacing={2}>
+                        <Stack spacing={2}>
                             {medicalteam.map(provider => (
                                 <Item 
                                 className='listItem'
                                 key={provider.id} 
                                 provider={provider}/>)
                             )}
-                        </Grid>
+                        </Stack>
                     :
                         <p>No medical providers</p>
                 }
@@ -54,8 +52,8 @@ function List() {
                     onClick={() => {history.push('/medicalteam/addnew')}} 
                     variant="outlined" 
                     size="small"
-                >
-                    Add New Provider
+                ><AddIcon/>
+                    Add
                 </Button>
             </Stack>
         </Container>
@@ -63,3 +61,75 @@ function List() {
 }
 
 export default List;
+
+
+
+
+
+
+// below works!
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector} from 'react-redux';
+// import { useHistory } from 'react-router-dom';
+// import Container from '@mui/material/Container';
+// import Button from '@mui/material/Button';
+// import Item from "../Item/Item";
+// import Stack from '@mui/material/Stack';
+// import Grid from '@mui/material/Unstable_Grid2';
+// import './List.css';
+
+// function List() {
+//     const dispatch = useDispatch();
+//     const history = useHistory();
+//     const medicalteam = useSelector(store => store.medicalteam.medicalteamReducer);
+
+//     useEffect(() => {
+//         dispatch({
+//             type: 'FETCH_MEDICAL_TEAM'
+//         })
+//     }, [])
+
+//     console.log('this is medicalteam from store:', medicalteam);
+
+//     return (
+//         <Container className='listContainer' maxWidth="sm">
+//             <Stack
+//                 className='listStack'
+//                 direction="column"
+//                 justifyContent="center"
+//                 alignItems="center"
+//                 spacing={2}
+//             >
+//                 <p className="listComponentTitle">Medical Providers</p>
+
+//                 {
+//                     medicalteam.length > 0 ?
+//                         <Grid 
+//                         className='listGrid'
+//                         container 
+//                         alignItems="stretch"
+//                         spacing={2}>
+//                             {medicalteam.map(provider => (
+//                                 <Item 
+//                                 className='listItem'
+//                                 key={provider.id} 
+//                                 provider={provider}/>)
+//                             )}
+//                         </Grid>
+//                     :
+//                         <p>No medical providers</p>
+//                 }
+                
+//                 <Button
+//                     onClick={() => {history.push('/medicalteam/addnew')}} 
+//                     variant="outlined" 
+//                     size="small"
+//                 >
+//                     Add New Provider
+//                 </Button>
+//             </Stack>
+//         </Container>
+//     )
+// }
+
+// export default List;
