c8f158142b8d6b13042397cbb7119ddf6542162f
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-19 15:46:24 -0500

    create insurance reducers, sagas, write insurance list, detail, and item components

--- src/components/InsuranceItem/InsuranceItem.jsx
+++ src/components/InsuranceItem/InsuranceItem.jsx
@@ -1,11 +1,31 @@
+import {useHistory} from 'react-router-dom';
 
-function InsuranceItem() {
+// MUI Imports
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
+function InsuranceItem({plan}) {
+    console.log('in insuranceItem and here it the prop passed in:', plan);
+    const history = useHistory();
 
+    const handleClick = () => {
+        console.log('in handleClick. item.id is:', plan.id);
+        history.push(`/insurance/detail/${plan.id}`);
+
+    } 
     return (
-        <>
-        <p>InsuranceItem Component</p>
-        </>
+        <Card 
+        sx={{ minWidth: 300 }} 
+        key={plan.id}
+        onClick={handleClick}
+        >
+            <CardContent>
+            <Typography>{plan.provider}</Typography>
+            <Typography variant="body2" >{plan.group}</Typography>
+            <Typography variant="body2" color="text.secondary">{plan.id_number}</Typography>
+            </CardContent>
+        </Card>    
     )
 }
 
--- src/components/InsuranceList/InsuranceList.jsx
+++ src/components/InsuranceList/InsuranceList.jsx
@@ -1,11 +1,59 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import InsuranceItem from "../InsuranceItem/InsuranceItem";
 
+// MUI Imports
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
 function InsuranceList() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const insurance = useSelector(store => store.insurance.insuranceReducer);
 
+    useEffect(() => {
+        dispatch({
+            type: 'FETCH_INSURANCE'
+        })
+    }, [])
 
+    console.log('in InsuranceList and insurance from store is:', insurance);
     return (
         <>
-        <InsuranceItem />
+        <Container maxWidth="sm">
+            <Stack
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+            >
+                <Typography>Insurance</Typography>
+                {
+                    insurance.length > 0 ?
+                        <Stack spacing={2}>
+                            {insurance.map(plan => (
+                                <InsuranceItem
+                                key={plan.id} 
+                                plan={plan}/>)
+                            )}
+                        </Stack>
+                    :
+                        <p>No insurance</p>
+                }
+                
+                <Button
+                    onClick={() => {history.push('/insurance/add')}} 
+                    variant="outlined" 
+                    size="small"
+                ><AddIcon/>
+                    Add
+                </Button>
+            </Stack>
+        </Container>
         </>
     )
 }
