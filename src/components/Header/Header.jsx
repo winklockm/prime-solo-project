947ff497ff0f5a3ce8a3fbb67e7b674f3cf704cb
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-10-23 10:58:21 -0500

    header and logo styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 
 function Header() {
   const user = useSelector((store) => store.user);
@@ -21,7 +22,8 @@ function Header() {
             alignItems="center"
             spacing={0}
             >
-              <h1 className='title'>TakeCare</h1>
+              {/* <h1 className='title'>TakeCare</h1> */}
+              <h1 className='large'><span className='takeLarge'>take</span ><span className='careLarge'>care</span></h1>
               <h2 className='subtitle'>caregiving. centralized.</h2>
             </Stack>
           </Container>
@@ -36,8 +38,9 @@ function Header() {
             alignItems="center"
             spacing={0}>
               {/* <h2 className='subtitle'>Picture</h2> */}
-              <Avatar src={user.patient_photo} sx={{width: 50, height: 50}} />
-              <h1 className='title'>TakeCare</h1>
+              <Avatar className='thumbnail' src={user.patient_photo} sx={{width: 60, height: 60}} />
+              {/* <h1 className='title'>TakeCare</h1> */}
+              <h1><span className='take'>take</span ><span className='care'>care</span></h1>
             </Stack>
           </Container>
         )}
